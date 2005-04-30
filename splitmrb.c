337ffea07678df6311a7bb43e282621478e1b5cc
Paul Wise <pabs3@bonedaddy.net>
2005-04-30 12:56:37 +0000

    Add copyright and licencing (GPL>=2) notices to all files.

--- splitmrb.c
+++ splitmrb.c
@@ -1,4 +1,24 @@
 /*
+helpdeco -- utility program to dissect Windows help files
+Copyright (C) 2005 Manfred Winterhoff
+
+This file is part of helpdeco; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA or visit:
+http://www.gnu.org
+*/
+
+/*
 SPLITMRB - splits MRB into SHG/BMP/WMF and SHG into BMP/WMF files - Version 1.5
 M.Winterhoff, Geschw.-Scholl-Ring 17, 38444 Wolfsburg, Germany, CIS 100326,2776
 
