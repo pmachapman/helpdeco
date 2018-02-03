294a6b62b26524cf56327136e1dcc3662b8c81fc
Ben Collver <bencollver@gmail.com>
2018-02-03 13:21:14 -0800

    Switch from u_int32_t to C99 standard uint32_t.

--- splitmrb.c
+++ splitmrb.c
@@ -237,9 +237,9 @@ int GetPackedByte(FILE *f) // RulLen decompression
 #endif
 typedef struct
 {
-  u_int8_t c1,c2,c3;
-  u_int16_t x,y,w,h;
-  u_int32_t hash;
+  uint8_t c1,c2,c3;
+  uint16_t x,y,w,h;
+  uint32_t hash;
 } HOTSPOT
 #if defined(__GNUC__) && __GNUC__ < 3
 __attribute__((packed))
